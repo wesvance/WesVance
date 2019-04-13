d76039ae568d3c5343dd525e7140f0c0c90694ea
Wesley Vance <wesley.vance@apollostu.com>
2019-04-13 00:58:49 -0400

    Updated images on main page

--- src/containers/welcome/WelcomePage.js
+++ src/containers/welcome/WelcomePage.js
@@ -6,10 +6,9 @@ import * as uiActions from '../../actions/uiActions';
 
 import MailingList from '../common/MailingList';
 
-import ThreeDayStartup from '../../assets/images/logos/3ds.png';
-import StartupAggieland from '../../assets/images/logos/startupaggieland.png';
-import SiliconValleyBank from '../../assets/images/logos/svb.png';
 import SXSW from '../../assets/images/logos/sxsw.png';
+import YouMeAndTheParks from '../../assets/images/logos/youmeandtheparks-horizontal-2.png';
+import Apollo from '../../assets/images/logos/apollo.png';
 
 
 class WelcomePage extends React.Component{
@@ -39,16 +38,19 @@ class WelcomePage extends React.Component{
               </div>
               <div className="row">
                 <div className="col-6 col-sm-3">
-                  <img src={SXSW} alt="SXSW" className="img-responsive"/>
+                  <a href="http://apollostu.com" target="_blank">
+                    <img src={Apollo} alt="ApolloStudios" className="img-responsive"/>
+                  </a>
                 </div>
-                <div className="col-6 col-sm-3">
-                  <img src={SiliconValleyBank} alt="SVB Trek" className="img-responsive"/>
+                <div className="col-6 col-sm-3 text-center">
+                  <a href="http://youmeandtheparks.com" target="_blank">
+                    <img src={YouMeAndTheParks} alt="YouMeAndTheParks" className="img-responsive"/>
+                  </a>
                 </div>
-                <div className="col-6 col-sm-3">
-                  <img src={ThreeDayStartup} alt="3DS" className="img-responsive"/>
-                </div>
-                <div className="col-6 col-sm-3">
-                  <img src={StartupAggieland} alt="Startup Aggieland" className="img-responsive"/>
+                <div className="col-6 col-sm-3 text-center">
+                  <a href="https://schedule.sxsw.com/2017/events/PP69025" target="_blank">
+                    <img src={SXSW} alt="SXSW" className="img-responsive"/>
+                  </a>
                 </div>
               </div>
               <div className="row">
